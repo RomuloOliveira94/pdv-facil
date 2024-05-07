ae5f77caabf0d5281d63c84a49b30005edcc08bd
Rômulo Augusto <romuloffall@gmail.com>
2024-05-07 00:30:56 -0300

    #implement cashier ccreate

--- app/Http/Controllers/CashierController.php
+++ app/Http/Controllers/CashierController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cashier;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class CashierController extends Controller
 {
-    public function index ()
+    public function index()
     {
         return Inertia::render('Cashiers/Index');
+    }
+
+    public function store()
+    {
+        $cashiers = Cashier::where('company_id', auth()->user()->company_id)
+            ->whereDate('created_at', now())
+            ->get();
+
+        if ($cashiers->count() > 0) {
+            return redirect()->route('sells.create');
+        }
+
+        Cashier::create([
+            'company_id' => auth()->user()->company_id,
+        ]);
+
+        return redirect()->route('sells.create');
     }
 }
