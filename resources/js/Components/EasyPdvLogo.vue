6c59c441097629231f854e180f8e6566bf87208f
Rômulo Augusto <romuloffall@gmail.com>
2024-06-15 19:33:50 -0300

    #include app logo

---
+++ resources/js/Components/EasyPdvLogo.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <img
+        src="/storage/logos/pdvfacil.png"
+        alt="pdv-fácil"
+        class="w-20 h-20 rounded-full object-cover"
+    />
+</template>
