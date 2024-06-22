3ec8f16a9d2ebeafa79fd27ee7f7d5fbe4ef9621
Rômulo Augusto <romuloffall@gmail.com>
2024-06-21 21:35:57 -0300

    #create admin layout

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import EasyPdvLogo from "@/Components/EasyPdvLogo.vue";
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <div
+        class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#FFF6E6]"
+    >
+        <div>
+            <Link href="/">
+                <EasyPdvLogo class="w-60 h-60 fill-current text-gray-500" />
+            </Link>
+        </div>
+
+        <div
+            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+        >
+            <slot />
+        </div>
+    </div>
+</template>
