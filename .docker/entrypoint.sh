47a5d2a0a9e8915b52017f9a79a4e794a109d7a8
Rômulo Augusto <romuloffall@gmail.com>
2024-09-02 20:44:03 -0300

    #add caddy

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -7,6 +7,10 @@ composer dump-autoload --no-interaction --no-dev --optimize
 echo "🎬 artisan commands"
 
 # 💡 Group into a custom command e.g. php artisan app:on-deploy
-npm run build
 php artisan migrate --no-interaction --force
 
+# Inicia o PHP-FPM em segundo plano
+php-fpm &
+
+# Inicia o Caddy
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
