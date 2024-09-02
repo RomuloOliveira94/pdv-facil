b89bb0429d6b1500d72aa2ab359a0fb85db04a09
Rômulo Augusto <romuloffall@gmail.com>
2024-09-02 12:16:43 -0300

    #remove build

--- public/build/assets/InputError-DZMNScGU.js
+++
@@ -1,1 +0,0 @@
-import{h as s,v as t,o as a,e as r,b as o,t as c}from"./app-Bq8-TzmE.js";const n={class:"text-sm text-red-600"},l={__name:"InputError",props:{message:{type:String}},setup(e){return(i,m)=>s((a(),r("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{l as _};
