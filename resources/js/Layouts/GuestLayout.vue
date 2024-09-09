d6fdb5d2d4998be35aefd19dd5157200aced4d77
Rômulo Augusto <romuloffall@gmail.com>
2024-09-09 16:45:41 -0300

    #implement login flow and links

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,12 +1,36 @@
 <script setup>
 import EasyPdvLogo from "@/Components/EasyPdvLogo.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
 import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
     <div
-        class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#FFF6E6]"
+        class="min-h-screen flex flex-col sm:justify-center items-center p-6 sm:pt-0 bg-[#FFF6E6]"
     >
+        <header>
+            <h1 class="text-4xl font-bold text-gray-800">
+                Crie sua conta ou faça login.
+            </h1>
+            <nav class="flex items-center gap-4 mt-4">
+                <Link
+                    :href="route('login')"
+                    class="underline text-lg text-gray-600 hover:text-gray-900"
+                >
+                    <PrimaryButton class="bg-white hover:bg-[#FFF6E6]"> Entrar </PrimaryButton>
+                </Link>
+                <Link>
+                    <Link
+                        :href="route('register')"
+                        class="underline text-lg text-gray-600 hover:text-gray-900"
+                    >
+                        <PrimaryButton class="bg-white hover:bg-[#FFF6E6]">
+                            Criar uma conta
+                        </PrimaryButton>
+                    </Link>
+                </Link>
+            </nav>
+        </header>
         <div>
             <Link href="/">
                 <EasyPdvLogo class="w-60 h-60 fill-current text-gray-500" />
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,11 +1,11 @@
 <script setup>
-import Checkbox from '@/Components/Checkbox.vue';
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import Checkbox from "@/Components/Checkbox.vue";
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { Head, Link, useForm } from "@inertiajs/vue3";
 
 defineProps({
     canResetPassword: {
@@ -17,14 +17,14 @@ defineProps({
 });
 
 const form = useForm({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
     remember: false,
 });
 
 const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
+    form.post(route("login"), {
+        onFinish: () => form.reset("password"),
     });
 };
 </script>
@@ -37,7 +37,7 @@ const submit = () => {
             {{ status }}
         </div>
 
-        <form @submit.prevent="submit">
+        <form @submit.prevent="submit" class="mx-2">
             <div>
                 <InputLabel for="email" value="Email" />
 
@@ -72,11 +72,13 @@ const submit = () => {
             <div class="block mt-4">
                 <label class="flex items-center">
                     <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600">Lembre-se de mim.</span>
+                    <span class="ms-2 text-sm text-gray-600"
+                        >Lembre-se de mim.</span
+                    >
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-between mt-4">
                 <Link
                     v-if="canResetPassword"
                     :href="route('password.request')"
@@ -85,10 +87,22 @@ const submit = () => {
                     Esqueceu a senha?
                 </Link>
 
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Entrar
-                </PrimaryButton>
+                <Link>
+                    <Link
+                        :href="route('register')"
+                        class="underline text-sm text-gray-600 hover:text-gray-900"
+                    >
+                        Criar uma conta
+                    </Link>
+                </Link>
             </div>
+            <PrimaryButton
+                class="w-full mt-4 text-center flex justify-center"
+                :class="{ 'opacity-25': form.processing }"
+                :disabled="form.processing"
+            >
+                Entrar
+            </PrimaryButton>
         </form>
     </GuestLayout>
 </template>
