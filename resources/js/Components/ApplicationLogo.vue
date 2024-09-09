593abef6269f09f92ca3367d5f90f27db3b63ff1
Rômulo Augusto <romuloffall@gmail.com>
2024-09-09 16:27:42 -0300

    #adjust application logo

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -3,7 +3,7 @@
 
 <template>
     <img
-        :src="'/storage/' + $page.props.logo.url"
+        :src="$page.props.logo.url"
         alt="pdv-fácil"
         class="w-16 h-16 rounded-full object-cover"
     />
